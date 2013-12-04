e16827f9340b455b1c02406f549c4e96b72ba4aa
t-korin <t.korin.git@gmail.com>
2013-12-04 13:13:40 +0900

    for eclipse

---
+++ Tools/src/Point3D/Point3DPlayer.java
@@ -0,0 +1,310 @@
+package Point3D;
+
+import java.util.ArrayList;
+
+import java.awt.Point;
+import java.awt.Color;
+import java.awt.Dimension;
+/*
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+*/
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JScrollBar;
+import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.AdjustmentEvent;
+
+import java.io.IOException;
+import java.io.File;
+
+import Data.*;
+
+public class Point3DPlayer extends JPanel implements ActionListener, AdjustmentListener{
+
+	public int WIDTH;
+	public int HEIGHT;
+
+	public static final int BUTTON_HEIGHT = 52;
+
+	public Point3DViewer pv;
+
+	public JPanel scrollPanel = new JPanel();
+	public ArrayList<JLabel> labelList = new ArrayList<JLabel>();
+	public ArrayList<JScrollBar> sb = new ArrayList<JScrollBar>();
+	public ArrayList<JPanel> sbPanel = new ArrayList<JPanel>();
+
+	public ArrayList<Integer> playPosition = new ArrayList<Integer>();
+
+	private JFileChooser jfc = new JFileChooser();
+
+	public boolean playing;
+	public ArrayList<Boolean> sentFlag = new ArrayList<Boolean>();
+
+	public ArrayList<MotionData> mdList = new ArrayList<MotionData>();
+	public ArrayList<Point[]> line = new ArrayList<Point[]>();
+
+	public void adjustmentValueChanged(AdjustmentEvent e){
+		for(int i = 0; i < mdList.size(); i++){
+			if(sb.get(i).getValue() != playPosition.get(i)){
+				pause();
+				playPosition.set(i,sb.get(i).getValue());
+			}
+		}
+		updatePoints();
+	}
+
+	public void setColor(String s, Color c){
+		try{
+			String searchStr = " " + s + ": ";
+			int idx = -1;
+			for(int i = 0; i < labelList.size(); i++){
+				if(labelList.get(i).getText().equals(searchStr)){
+					idx = i;
+					break;
+				}
+			}
+			if(idx < 0) return;
+			JLabel tmp = labelList.get(idx);
+			pv.setColor(idx,c);
+			tmp.setForeground(c);
+			labelList.set(idx,tmp);
+		}catch(IndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void actionPerformed(ActionEvent e){
+		String s = e.getActionCommand();
+		if(s == "Play"){
+			play();
+		}
+		if(s == "Stop"){
+			stop();
+		}
+		if(s == "Pause"){
+			pause();
+		}
+		if(s == "Reset View"){
+			pv.setDefaultView();
+		}
+	}
+
+	public Point3DPlayer(){
+		this(640,640);
+	}
+
+	public Point3DPlayer(int width, int height) {
+		WIDTH = width;
+		HEIGHT = height;
+		initGUI();
+	}
+
+	public void initGUI(){
+		JButton pl = new JButton("Play");
+		pl.addActionListener(this);
+		JButton pa = new JButton("Pause");
+		pa.addActionListener(this);
+		JButton st = new JButton("Stop");
+		st.addActionListener(this);
+		JPanel playButtons = new JPanel();
+		playButtons.setLayout(new GridLayout(1,3));
+
+		playButtons.add(pl);
+		playButtons.add(pa);
+		playButtons.add(st);
+
+		JButton reset = new JButton("Reset View");
+		reset.addActionListener(this);
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new GridLayout(2,1));
+		buttons.add(playButtons);
+		buttons.add(reset);
+		setLayout(new BorderLayout());
+
+		pv = new Point3DViewer(WIDTH, HEIGHT-BUTTON_HEIGHT);
+		add(pv,BorderLayout.NORTH);
+		add(scrollPanel,BorderLayout.CENTER);
+		add(buttons,BorderLayout.SOUTH);
+
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+
+	public void reSize(int w, int h){
+		if(w<0||h<0)return;
+		WIDTH = w;
+		HEIGHT = h;
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		pv.reSize(WIDTH,HEIGHT-BUTTON_HEIGHT-sb.size()*20-70);
+
+		scrollPanel.setLayout(new GridLayout(sb.size(),1));
+		scrollPanel.revalidate();
+		scrollPanel.repaint();
+		revalidate();
+		repaint();
+	}
+
+	public void setMovable(String s, boolean b){
+		pv.setMovable(" "+s+": ",b);
+	}
+
+	public void loadData(String s){
+		addData(new File("./"+s));
+	}
+
+	public void addData(String l, MotionData md, Color c, boolean b){
+		JScrollBar sbTmp = new JScrollBar(JScrollBar.HORIZONTAL);
+		sbTmp.setMaximum(md.size()-1+sbTmp.getVisibleAmount());
+		sbTmp.addAdjustmentListener(this);
+		JLabel label = new JLabel(" "+l+": ");
+		label.setForeground(c);
+		label.setBackground(Color.BLACK);
+		label.setOpaque(true);
+		labelList.add(label);
+		JPanel paneTmp = new JPanel();
+		paneTmp.setLayout(new BorderLayout());
+		paneTmp.add(label,BorderLayout.WEST);
+		paneTmp.add(sbTmp,BorderLayout.CENTER);
+		sb.add(sbTmp);
+		scrollPanel.add(paneTmp);
+		playPosition.add(new Integer(0));
+		mdList.add(md);
+		line.add(md.getLine());
+		sentFlag.add(false);
+		reSize(WIDTH,HEIGHT);
+		sendPoints(c,b);
+	}
+
+	public void rmData(String l){
+		String tmp = " " + l + ": ";
+		for(int i = 0; i < labelList.size(); i++){
+			if(tmp.equals(labelList.get(i).getText())){
+				scrollPanel.remove(i);
+				labelList.remove(i);
+				playPosition.remove(i);
+				mdList.remove(i);
+				line.remove(i);
+				sentFlag.remove(i);
+				sb.remove(i);
+				pv.rmPoints(tmp);
+				reSize(WIDTH,HEIGHT);
+				return;
+			}
+		}
+	}
+
+	public void addData(File f){
+		MotionData tmp = new MotionData();
+		if(!tmp.readFile(f)) return;
+		JScrollBar sbTmp = new JScrollBar(JScrollBar.HORIZONTAL);
+		sbTmp.setMaximum(tmp.size()-1+sbTmp.getVisibleAmount());
+		sbTmp.addAdjustmentListener(this);
+		sb.add(sbTmp);
+		scrollPanel.setLayout(new GridLayout(sb.size(),1));
+		scrollPanel.add(sbTmp);
+		playPosition.add(new Integer(0));
+		mdList.add(tmp);
+		line.add(tmp.getLine());
+		sentFlag.add(false);
+		sendPoints();
+		scrollPanel.validate();
+		scrollPanel.repaint();
+		reSize(WIDTH,HEIGHT);
+	}
+
+	public void pause(){
+		playing = false;
+	}
+
+	public void stop(){
+		playing = false;
+		for(int i = 0; i < mdList.size(); i++){
+			playPosition.set(i,0);
+			sb.get(i).setValue(0);
+		}
+		updatePoints();
+	}
+
+	public void updatePoints(){
+		for(int i = 0; i < mdList.size(); i++){
+			Vec3D[] tmp = mdList.get(i).get(playPosition.get(i));
+			if(tmp == null){ continue; }
+			pv.updatePoints(labelList.get(i).getText(),tmp);
+		}
+	}
+
+	public void printStatus(){
+		for(int i = 0; i < mdList.size(); i++){
+			System.out.println(i+"= position:"+playPosition.get(i)+"/"+mdList.get(i).size()+", bar:"+sb.get(i).getValue()+"/"+sb.get(i).getMaximum());
+		}
+	}
+
+	public void next(){
+		boolean end = true;
+		for(int i = 0; i < mdList.size(); i++){
+			if(playPosition.get(i) < mdList.get(i).size()-1){
+				end = false;
+				playPosition.set(i,playPosition.get(i)+1);
+				sb.get(i).setValue(playPosition.get(i));
+			}
+		}
+		updatePoints();
+		if(end) pause();
+	}
+
+	public void sendPoints(){
+		for(int i = 0; i < mdList.size(); i++){
+			if(playPosition.get(i) >= mdList.get(i).size()) continue; //playPosition = 0;
+			if(!sentFlag.get(i)){
+				pv.addPoints(labelList.get(i).getText(),mdList.get(i).get(playPosition.get(i)),line.get(i),Color.WHITE,true);
+				sentFlag.set(i,true);
+			}
+		}
+	}
+
+	public void sendPoints(Color c, boolean b){
+		for(int i = 0; i < mdList.size(); i++){
+			if(playPosition.get(i) >= mdList.get(i).size()) continue; //playPosition = 0;
+			if(!sentFlag.get(i)){
+				pv.addPoints(labelList.get(i).getText(),mdList.get(i).get(playPosition.get(i)),line.get(i),c,b);
+				sentFlag.set(i,true);
+			}
+		}
+	}
+
+	public void play(){
+		boolean allEnd = true;
+		for(int i = 0; i < mdList.size(); i++){
+			if(playPosition.get(i) < mdList.get(i).size()-1){
+				allEnd = false;
+				break;
+			}
+		}
+		if(allEnd){
+			for(int i = 0; i < mdList.size(); i++){
+				playPosition.set(i,0);
+				sb.get(i).setValue(0);
+			}
+		}
+		playing = true;
+	}
+
+	public void update(){
+		if(playing){
+			next();
+		}
+		pv.update();
+	}
+
+}
---
+++ Tools/src/Point3D/Point3DViewer.java
@@ -0,0 +1,408 @@
+package Point3D;
+
+import java.lang.IndexOutOfBoundsException;
+
+import java.util.ArrayList;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Point;
+
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JMenuItem;
+
+import Data.*;
+
+public class Point3DViewer extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener{
+
+	public int currentID = 0;
+
+	public double SIZE_UNIT_BY_PERSPECTIVE;
+	public Vec3D SIZE_ORIGIN;
+
+	public static final double MIN_ZOOM = 0;
+	public double zoom = 1;
+
+	public int mouseX, mouseY;
+
+	public boolean endDrawnConvert = true;
+
+	public ArrayList<Vec3D[]> point = new ArrayList<Vec3D[]>();
+	public ArrayList<Vec3D[]> rotatedPoint = new ArrayList<Vec3D[]>();
+	public ArrayList<Vec3D> drawnPoint= new ArrayList<Vec3D>();
+	public ArrayList<Point[]> line = new ArrayList<Point[]>();
+	public ArrayList<Point> ids = new ArrayList<Point>();
+	public ArrayList<Color> color = new ArrayList<Color>();
+
+	public ArrayList<Quaternion> qu = new ArrayList<Quaternion>();
+
+	public ArrayList<Vec3D> origin = new ArrayList<Vec3D>();
+	public Vec3D sharedOrigin;
+	public static final Vec3D first = new Vec3D(0,0,-1);
+	public ArrayList<String> names = new ArrayList<String>();
+	public ArrayList<Boolean> selected = new ArrayList<Boolean>();
+
+	private JPopupMenu popupMenu;
+	private JPanel menuPanel;
+
+	private int WIDTH;
+	private int HEIGHT;
+
+	private Image dbImage;
+	private Graphics dbg;
+
+	public void reset(){
+		point.clear();
+		rotatedPoint.clear();
+		color.clear();
+		line.clear();
+		qu.clear();
+		names.clear();
+		selected.clear();
+		zoom = 1;
+	}
+
+	public void setDefaultView(){
+		zoom = Double.MAX_VALUE;
+		for(int i = 0; i < point.size(); i++){
+			double minX = Double.MAX_VALUE;
+			double minY = Double.MAX_VALUE;
+			double maxX = Double.MIN_VALUE;
+			double maxY = Double.MIN_VALUE;
+			Vec3D[] v = point.get(i);
+			for(int j = 0; j < v.length; j++){
+				if(minX > v[j].getX()) minX = v[j].getX();
+				if(maxX < v[j].getX()) maxX = v[j].getX();
+				if(minY > v[j].getY()) minY = v[j].getY();
+				if(maxY < v[j].getY()) maxY = v[j].getY();
+			}
+			double tmpX = (WIDTH-50)/(maxX-minX);
+			double tmpY = (HEIGHT-50)/(maxY-minY);
+			double tmpZoom = (tmpX < tmpY ? tmpX : tmpY);
+			if(zoom > tmpZoom) zoom = tmpZoom;
+			origin.set(i,new Vec3D(((-minX-maxX)*zoom+WIDTH)/2.0,((minY+maxY)*zoom+HEIGHT)/2.0,0));
+			qu.set(i,new Quaternion(1,0,0,0));
+		}
+	}
+
+	public void updatePoints(int i, Vec3D[] v){
+		point.set(i,v);
+		autoRotate();
+	}
+
+	public void updatePoints(String s, Vec3D[] v){
+		int idx = nameToIdx(s);
+		if(idx == -1) return;
+		updatePoints(idx,v);
+	}
+
+	public void updatePoints(int i, Vec3D[] v, Point[] l, Color c){
+		point.set(i,v);
+		line.set(i,l);
+		color.set(i,c);
+		autoRotate();
+	}
+
+	public int nameToIdx(String s){
+		for(int i = 0; i < point.size(); i++){
+			if(s.equals(names.get(i))){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public void updatePoints(String s, Vec3D[] v, Point[]l, Color c){
+		int idx = nameToIdx(s);
+		if(idx == -1) return;
+		point.set(idx,v);
+		line.set(idx,l);
+		color.set(idx,c);
+		autoRotate();
+	}
+
+	public void addPoints(String s, Vec3D[] v, Point[] l, Color c, boolean b){
+		double minX = Double.MAX_VALUE;
+		double minY = Double.MAX_VALUE;
+		double maxX = Double.MIN_VALUE;
+		double maxY = Double.MIN_VALUE;
+		for(int i = 0; i < v.length; i++){
+			if(minX > v[i].getX()) minX = v[i].getX();
+			if(maxX < v[i].getX()) maxX = v[i].getX();
+			if(minY > v[i].getY()) minY = v[i].getY();
+			if(maxY < v[i].getY()) maxY = v[i].getY();
+		}
+		double tmpX = (WIDTH-50)/(maxX-minX);
+		double tmpY = (HEIGHT-50)/(maxY-minY);
+		double tmpZoom = (tmpX < tmpY ? tmpX : tmpY);
+		if(zoom > tmpZoom || point.size() == 0) zoom = tmpZoom;
+		names.add(s);
+		point.add(v);
+		rotatedPoint.add(v.clone());
+		color.add(c);
+		line.add(l);
+		selected.add(b);
+		origin.add(new Vec3D(((-minX-maxX)*zoom+WIDTH)/2.0,((minY+maxY)*zoom+HEIGHT)/2.0-10,0));
+		qu.add(new Quaternion(1,0,0,0));
+	}
+
+	public void setColor(int i, Color c){
+		color.set(i,c);
+	}
+
+	public void rmPoints(String s){
+		int i = nameToIdx(s);
+		if(i == -1) return;
+		names.remove(i);
+		point.remove(i);
+		rotatedPoint.remove(i);
+		color.remove(i);
+		line.remove(i);
+		selected.remove(i);
+		origin.remove(i);
+		qu.remove(i);
+	}
+
+	public Point3DViewer(){
+		this(480,480);
+	}
+
+	public Point3DViewer(int width, int height) {
+		WIDTH = width;
+		HEIGHT = height;
+
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		addMouseWheelListener(this);
+
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		sharedOrigin = new Vec3D(WIDTH/2,HEIGHT/2,0);
+		SIZE_UNIT_BY_PERSPECTIVE = 50;
+		SIZE_ORIGIN = new Vec3D(0,0,300);
+	}
+
+	public void reSize(int w, int h){
+		if(w<0||h<0)return;
+		WIDTH = w;
+		HEIGHT = h;
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		revalidate();
+		repaint();
+		dbImage = createImage(WIDTH,HEIGHT);
+		dbg = dbImage.getGraphics();
+	}
+
+	public void setSizeOrigin(Vec3D v){ SIZE_ORIGIN = v; }
+	public void setSizeUnit(double d){ SIZE_UNIT_BY_PERSPECTIVE = d; }
+
+	public synchronized void update() {
+		if(dbImage == null){
+			dbImage = createImage(WIDTH,HEIGHT);
+			if(dbImage == null){
+				System.out.println("dbImage is null");
+				return;
+			}else{
+				dbg = dbImage.getGraphics();
+			}
+		}
+		dbg.setColor(Color.BLACK);
+		dbg.fillRect(0,0,WIDTH,HEIGHT);
+
+		switch(Option.perspective){
+		case Option.ONE_POINT_PERSPECTIVE:
+			setPointByOnePointPerspective();
+			break;
+		case Option.NO_PERSPECTIVE:
+			setPoint();
+			break;
+		}
+		drawPointAndLine();
+
+		draw();
+	}
+
+	public void autoRotate(){
+		for(int i = 0; i < point.size(); i++){
+			rotatePoints(qu.get(i), point.get(i), i);
+		}
+	}
+
+	private void setPointByOnePointPerspective(){
+		drawnPoint.clear();
+		ids.clear();
+		for(int i = 0; i < rotatedPoint.size(); i++){
+			Vec3D[] rp = rotatedPoint.get(i);
+			for(int j = 0; j < rp.length; j++){
+				Vec3D tmp = rp[j].sub(Option.onePP);
+				tmp = tmp.times(1.0-(rp[j].getZ()/Option.onePP.getZ()));
+				tmp = tmp.add(Option.onePP);
+				boolean insert = false;
+				for(int k = 0; k < drawnPoint.size(); k++){
+					if(tmp.getZ() > drawnPoint.get(k).getZ()){
+						drawnPoint.add(k,tmp);
+						ids.add(k,new Point(i,j));
+						insert = true;
+						break;
+					}
+				}
+				if(!insert){
+					drawnPoint.add(tmp);
+					ids.add(new Point(i,j));
+				}
+			}
+		}
+	}
+
+	private void setPoint(){
+		drawnPoint.clear();
+		ids.clear();
+		for(int i = 0; i < rotatedPoint.size(); i++){
+			Vec3D[] rp = rotatedPoint.get(i);
+			for(int j = 0; j < rp.length; j++){
+				boolean insert = false;
+				for(int k = 0; k < drawnPoint.size(); k++){
+					if(rp[j].getZ() > drawnPoint.get(k).getZ()){
+						drawnPoint.add(k,rp[j]);
+						ids.add(k,new Point(i,j));
+						insert = true;
+						break;
+					}
+				}
+				if(!insert){
+					drawnPoint.add(rp[j]);
+					ids.add(new Point(i,j));
+				}
+			}
+		}
+	}
+
+	public void drawPoint(Vec3D p, Color c){
+		dbg.setColor(c);
+		int x,y;
+		x = (int)(sharedOrigin.getX()+p.getX()*zoom);
+		y = (int)(sharedOrigin.getY()-p.getY()*zoom);
+		dbg.fillOval(x-2,y-2,8,8);
+	}
+
+	public void setMovable(String s, boolean b){
+		int i = nameToIdx(s);
+		if(i == -1) return;
+		selected.set(i,b);
+	}
+
+	private void drawPointAndLine(){
+		try{
+			for(int i = 0; i < drawnPoint.size(); i++){
+				Point id = new Point(ids.get(i).x, ids.get(i).y);
+				Vec3D dp = drawnPoint.get(i).clone();
+				int x,y;
+				x = (int)(origin.get(id.x).getX()+dp.getX()*zoom);
+				y = (int)(origin.get(id.x).getY()-dp.getY()*zoom);
+				int size = (int)((SIZE_ORIGIN.getZ()-dp.getZ())/SIZE_UNIT_BY_PERSPECTIVE);
+				dbg.setColor(color.get(id.x));
+				dbg.fillOval(x-2,y-2,size,size);
+				for(int j = 0; j < line.get(id.x).length; j++){
+					Vec3D tmpV;
+					Point tmpL = line.get(id.x)[j];
+					if(tmpL.x == id.y) tmpV = drawnPoint.get(ids.indexOf(new Point(id.x,tmpL.y))).clone();
+					else continue;
+					int x2,y2;
+					x2 = (int)(origin.get(id.x).getX()+tmpV.getX()*zoom);
+					y2 = (int)(origin.get(id.x).getY()-tmpV.getY()*zoom);
+					int size2 = (int)((SIZE_ORIGIN.getZ()-tmpV.getZ())/SIZE_UNIT_BY_PERSPECTIVE);
+					dbg.drawLine(x+size/2,y+size/2,x2+size2/2,y2+size2/2);
+				}
+			}
+		}catch(IndexOutOfBoundsException e){
+			System.out.println("IndexOut");
+		}catch(NullPointerException e){
+			System.out.println("NullPointer");
+		}
+	}
+
+	private void rotatePoints(Quaternion q, Vec3D[] p, int id){
+		Vec3D[] rp = rotatedPoint.get(id);
+		if(rp == null || rp.length != p.length) rp = new Vec3D[p.length];
+		for(int i = 0; i < p.length; i++){
+			rp[i] = q.rotate(p[i].sub(Option.rot));
+			rp[i] = rp[i].add(Option.rot);
+		}
+	}
+
+	public void draw(){
+		drawPointAndLine();
+		if(Option.viewR) drawPoint(Option.rot,Color.RED);
+		if(Option.viewO) drawPoint(Option.onePP,Color.BLUE);
+		try{
+			Graphics g = getGraphics();
+			if((g != null) && (dbImage != null)){
+				g.drawImage(dbImage,0,0,null);
+			}
+			Toolkit.getDefaultToolkit().sync();
+			if(g != null){
+				g.dispose();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * mouse methods
+	 */
+	public void mousePressed(MouseEvent e){
+		mouseX = e.getX();
+		mouseY = e.getY();
+	}
+	public void mouseDragged(MouseEvent e){
+		int mx = e.getX();
+		int my = e.getY();
+		double xDiff = mx-mouseX;
+		double yDiff = my-mouseY;
+		if((e.getModifiers() & MouseEvent.BUTTON1_MASK) != 0){
+			double abs = Math.max(WIDTH,HEIGHT);
+			Vec3D from = new Vec3D(0,0,-1);
+			Vec3D to = new Vec3D(xDiff,-yDiff,-Math.sqrt(abs*abs-(xDiff*xDiff+yDiff*yDiff)));
+			Quaternion rot = new Quaternion(from,to);
+			for(int i = 0; i < selected.size(); i++){
+				if(selected.get(i)){
+					qu.set(i,qu.get(i).mul(rot));
+				}
+			}
+			autoRotate();
+		}
+		if((e.getModifiers() & MouseEvent.BUTTON3_MASK) != 0){
+			for(int i = 0; i < selected.size(); i++){
+				if(selected.get(i)){
+					origin.set(i,origin.get(i).add(new Vec3D(xDiff,yDiff,0)));
+				}
+			}
+		}
+		mouseX = mx;
+		mouseY = my;
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent e){
+		zoom -= e.getWheelRotation()/10.0;
+		if(zoom < MIN_ZOOM) zoom = MIN_ZOOM;
+	}
+	public void mouseEntered(MouseEvent e){
+	}
+	public void mouseExited(MouseEvent e){
+	}
+	public void mouseReleased(MouseEvent e){
+	}
+	public void mouseMoved(MouseEvent e){
+	}
+	public void mouseClicked(MouseEvent e){
+	}
+}
